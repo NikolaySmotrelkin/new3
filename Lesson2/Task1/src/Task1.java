85bf24687f1cd0206fded8e687164efc908f4b04
nikolay.smotrelkin <test@test.ru>
2020-09-04 17:42:13 +0300

    Сделан 1 урок

--- Lesson2/Task1/src/Task1.java
+++ Lesson2/Task1/src/Task1.java
@@ -1,12 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Task1 {
+
     public static void main(String[] args) {
-        System.out.println("Введите число");
-        BufferedReader bufReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите целое число");
+        Scanner scanner = new Scanner(System.in);
+
         try {
-            String str = bufReader.readLine();
+            int value = scanner.nextInt();
+            String str = Integer.toString(value);
             char chr = str.charAt(str.length() - 1);
             System.out.println("Последняя цифра в числе: " + chr);
         } catch (Exception e) {
